9ed1a0ef2afcc4ff0953d0ac540a8dce73465bf7
avmolaei <avmolaei@gmail.com>
2023-08-02 19:26:15 +0200

    fixed wrong file uploaded, oops

--- FIRMWARE FILES/AV_RK_MK_II_FIRMWARE/V2/AVLABS_AV_RK_MKII.ino
+++ FIRMWARE FILES/AV_RK_MK_II_FIRMWARE/V2/AVLABS_AV_RK_MKII.ino
@@ -2,6 +2,7 @@
 AVLABS_AV_RK_MKII.ino, the firmware of the AV-RK MK II keyboard.
     Copyright (C) 2023  AVlabs (Avesta MOLAEI)
 
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -20,17 +21,28 @@ AVLABS_AV_RK_MKII.ino, the firmware of the AV-RK MK II keyboard.
 //------------------LIBRARIES------------------
 //PCA9505_9506.h: Lib for the NXP PCA9505 IO Expander through I2C
 //Keyboard.h:     Classic Arduino Keyboard lib
+//Wire, Mouse and Pimoroni libs for the trackball
 #include <PCA9505_9506.h>
 #include <Keyboard.h>
+#include <Wire.h>
+#include "Mouse.h"
+#include <pimoroniTrackball.h> 
 //---------------------------------------------
 
 
-//------------------DEFINES--------------------
-#define NUM_KEYS_NUMPAD 40
-// #define NUM_KEYS_LEFT 40
-// #define NUM_KEYS_RIGHT 40  //Maybe i should just have a general NUM_KEYS as all of them are 40 lmao
-//---------------------------------------------
+//------------------DEFINES-------------------
+#define INITIAL_DELAY 150
+#define REPEAT_DELAY 25
+#define MOUSE_LEFT_CLICK  0xFF
+#define MOUSE_RIGHT_CLICK 0xFE
+#define MOUSE_SCROLL_UP   0xFD
+#define MOUSE_SCROLL_DOWN 0xFC
+#define FUNCTION_KEY 0xFB
+#define KEY_CHEVRON_L 236
+#define KEY_CHEVRON_R 236
+#define KEY_SCREENSHOT 0xFA
 
+//---------------------------------------------
 
 //------------------PCA OBJECTS----------------
 PCA9505_06 NUMPAD;
@@ -39,231 +51,216 @@ PCA9505_06 RIGHT;
 //---------------------------------------------
 
 
-//------------------TYPEDEFS-------------------
-typedef struct{
-  PCA9505_06* expander;
-  uint8_t pin;
-  uint8_t key;
-} Key; //KEY{&EXPANDER, PIN, CODE}
-//---------------------------------------------
-
-
-//------------------KEYMAPS--------------------
-Key NUMPADMAP[] = {
-  {&NUMPAD, 0,  0xCE},
-  {&NUMPAD, 1,  0xCF},
-  {&NUMPAD, 2,  0xD0},
-  {&NUMPAD, 3,  0xDB},
-  {&NUMPAD, 4,  0xDC},
-  {&NUMPAD, 5,  0xDD},
-  {&NUMPAD, 6,  0xDE},
-  {&NUMPAD, 7,  0xDA},
-
-  {&NUMPAD, 8,  0xD1},
-  {&NUMPAD, 9,  0xD2},
-  {&NUMPAD, 10, 0xD3},
-  {&NUMPAD, 11, 0xE7},
-  {&NUMPAD, 12, 0xE8},
-  {&NUMPAD, 13, 0xE9},
-  {&NUMPAD, 14, 0xDF},
-  {&NUMPAD, 15, 0xDA},
-
-  {&NUMPAD, 16, 0xD4},
-  {&NUMPAD, 17, 0xD5},
-  {&NUMPAD, 18, 0xD6},
-  {&NUMPAD, 19, 0xE4},
-  {&NUMPAD, 20, 0xE5},
-  {&NUMPAD, 21, 0xE6},
-  {&NUMPAD, 22, 0xDF},
-  {&NUMPAD, 23, 0xDA},
-
-  {&NUMPAD, 24, 0xDA},
-  {&NUMPAD, 25, 0xDA},
-  {&NUMPAD, 26, 0xDA},
-  {&NUMPAD, 27, 0xE1},
-  {&NUMPAD, 28, 0xE2},
-  {&NUMPAD, 29, 0xE3},
-  {&NUMPAD, 30, 0xE0},
-  {&NUMPAD, 31, 0xDA},
-
-  {&NUMPAD, 32, 0xD8},
-  {&NUMPAD, 33, 0xD9},
-  {&NUMPAD, 34, 0xD7},
-  {&NUMPAD, 35, 0xEA},
-  {&NUMPAD, 36, 0xEA},
-  {&NUMPAD, 37, 0xEB},
-  {&NUMPAD, 38, 0xE0},
-  {&NUMPAD, 39, 0xDA}
-}; //NUMPAD keymap
-/*
-Key LEFTMAP[] = {
-  {&LEFT, 0,  0xCE},
-  {&LEFT, 1,  0xCF},
-  {&LEFT, 2,  0xD0},
-  {&LEFT, 3,  0xDB},
-  {&LEFT, 4,  0xDC},
-  {&LEFT, 5,  0xDD},
-  {&LEFT, 6,  0xDE},
-  {&LEFT, 7,  0xDA},
-
-  {&LEFT, 8,  0xD1},
-  {&LEFT, 9,  0xD2},
-  {&LEFT, 10, 0xD3},
-  {&LEFT, 11, 0xE7},
-  {&LEFT, 12, 0xE8},
-  {&LEFT, 13, 0xE9},
-  {&LEFT, 14, 0xDF},
-  {&LEFT, 15, 0xDA},
-
-  {&LEFT, 16, 0xD4},
-  {&LEFT, 17, 0xD5},
-  {&LEFT, 18, 0xD6},
-  {&LEFT, 19, 0xE4},
-  {&LEFT, 20, 0xE5},
-  {&LEFT, 21, 0xE6},
-  {&LEFT, 22, 0xDF},
-  {&LEFT, 23, 0xDA},
-
-  {&LEFT, 24, 0xDA},
-  {&LEFT, 25, 0xDA},
-  {&LEFT, 26, 0xDA},
-  {&LEFT, 27, 0xE1},
-  {&LEFT, 28, 0xE2},
-  {&LEFT, 29, 0xE3},
-  {&LEFT, 30, 0xE0},
-  {&LEFT, 31, 0xDA},
-
-  {&LEFT, 32, 0xD8},
-  {&LEFT, 33, 0xD9},
-  {&LEFT, 34, 0xD7},
-  {&LEFT, 35, 0xEA},
-  {&LEFT, 36, 0xEA},
-  {&LEFT, 37, 0xEB},
-  {&LEFT, 38, 0xE0},
-  {&LEFT, 39, 0xDA}
-}; //LEFT keymap
-
-Key RIGHTMAP[] = {
-  {&RIGHT, 0,  0xCE},
-  {&RIGHT, 1,  0xCF},
-  {&RIGHT, 2,  0xD0},
-  {&RIGHT, 3,  0xDB},
-  {&RIGHT, 4,  0xDC},
-  {&RIGHT, 5,  0xDD},
-  {&RIGHT, 6,  0xDE},
-  {&RIGHT, 7,  0xDA},
-
-  {&RIGHT, 8,  0xD1},
-  {&RIGHT, 9,  0xD2},
-  {&RIGHT, 10, 0xD3},
-  {&RIGHT, 11, 0xE7},
-  {&RIGHT, 12, 0xE8},
-  {&RIGHT, 13, 0xE9},
-  {&RIGHT, 14, 0xDF},
-  {&RIGHT, 15, 0xDA},
-
-  {&RIGHT, 16, 0xD4},
-  {&RIGHT, 17, 0xD5},
-  {&RIGHT, 18, 0xD6},
-  {&RIGHT, 19, 0xE4},
-  {&RIGHT, 20, 0xE5},
-  {&RIGHT, 21, 0xE6},
-  {&RIGHT, 22, 0xDF},
-  {&RIGHT, 23, 0xDA},
-
-  {&RIGHT, 24, 0xDA},
-  {&RIGHT, 25, 0xDA},
-  {&RIGHT, 26, 0xDA},
-  {&RIGHT, 27, 0xE1},
-  {&RIGHT, 28, 0xE2},
-  {&RIGHT, 29, 0xE3},
-  {&RIGHT, 30, 0xE0},
-  {&RIGHT, 31, 0xDA},
-
-  {&RIGHT, 32, 0xD8},
-  {&RIGHT, 33, 0xD9},
-  {&RIGHT, 34, 0xD7},
-  {&RIGHT, 35, 0xEA},
-  {&RIGHT, 36, 0xEA},
-  {&RIGHT, 37, 0xEB},
-  {&RIGHT, 38, 0xE0},
-  {&RIGHT, 39, 0xDA}
-}; //RIGHT keymap
-//---------------------------------------------
-*/
-
 //--------------------VARS---------------------
+const uint8_t numKeys = 40;
+
+char keyMapNUMPAD[numKeys] = { 
+  KEY_PRINT_SCREEN, KEY_SCROLL_LOCK, KEY_PAUSE,        KEY_NUM_LOCK, KEY_KP_SLASH, KEY_KP_ASTERISK,  KEY_KP_MINUS,           KEY_F13, 
+  KEY_INSERT,       KEY_HOME,        KEY_PAGE_UP,      KEY_KP_7,     KEY_KP_8,      KEY_KP_9,        KEY_KP_PLUS,            KEY_F13,
+  KEY_DELETE,       KEY_END,         KEY_PAGE_DOWN,    KEY_KP_4,     KEY_KP_5,      KEY_KP_6,        KEY_KP_PLUS,            KEY_F13,
+  KEY_F13,          KEY_UP_ARROW,    KEY_F13,          KEY_KP_1,     KEY_KP_2,      KEY_KP_3,        KEY_KP_ENTER,           KEY_F13,
+  KEY_LEFT_ARROW,   KEY_DOWN_ARROW,  KEY_RIGHT_ARROW,  KEY_KP_0,     KEY_KP_0,      KEY_KP_ENTER,    KEY_KP_DOT,             KEY_F13
+};
+
+char keyMapNUMPADALT[numKeys] = {0};
+
+char keyMapLEFT[numKeys] = {
+  KEY_ESC,           '1',            '2',              '3',          '4',           '5',             '6',                    KEY_F13,
+  KEY_TAB,           'q',            'w',              'e',          'r',           't',             '`'/*KEY_SUPERSCRIPT_TWO*/,    KEY_F13, 
+  KEY_CAPS_LOCK,     'a',            's',              'd',          'f',           'g',              MOUSE_LEFT_CLICK/*'CLIC GAUCHE'*/,          KEY_F13, 
+  KEY_LEFT_SHIFT,    KEY_CHEVRON_L,      'z',              'x',          'c',           'v',             MOUSE_RIGHT_CLICK/*'CLIC DROIT'*/,           KEY_F13, 
+  KEY_LEFT_CTRL,      KEY_LEFT_GUI,  KEY_LEFT_ALT,   KEY_F13/* FONCTION*/,     KEY_SCREENSHOT/*SCREENSHOT*/,    ' ',             KEY_RETURN,           KEY_F13
+}; 
+
+char keyMapLEFTALT[numKeys] = {
+  KEY_ESC,           KEY_F1,            KEY_F2,              KEY_F3,          KEY_F4,           KEY_F5,             KEY_F6,                    KEY_F13,
+  KEY_TAB,           'q',            'w',              'e',          'r',           't',             '`'/*KEY_SUPERSCRIPT_TWO*/,    KEY_F13, 
+  KEY_CAPS_LOCK,     'a',            's',              'd',          'f',           'g',              MOUSE_SCROLL_UP/*'CLIC GAUCHE'*/,          KEY_F13, 
+  KEY_LEFT_SHIFT,    KEY_F13/*'chevron'*/,      'z',              'x',          'c',           'v',             MOUSE_SCROLL_DOWN/*'CLIC DROIT'*/,           KEY_F13, 
+  KEY_LEFT_CTRL,      KEY_LEFT_GUI,  KEY_LEFT_ALT,   KEY_F13/* FONCTION*/,     KEY_F13/*SCREENSHOT*/,    ' ',             KEY_RETURN,           KEY_F13
+};
+
+char keyMapRIGHT[numKeys] = {
+  '7',                '8',            '9',              '0',          '-',           '=',            '[',                    KEY_F13,
+  KEY_F13,          'y',            'u',              'i',          'o',           'p',            ']',                    KEY_F13, 
+  KEY_BACKSPACE,     'h',            'j',              'k',          'l',           ';',             '\'',                    KEY_F13, 
+  KEY_DELETE,        'b',            'n',              'm',          ',',           '.',            '/',                    KEY_F13, 
+  KEY_RETURN,             ' ',          KEY_RIGHT_ALT,           KEY_RIGHT_GUI,       KEY_MENU,          KEY_RIGHT_CTRL,          '\\',                    KEY_F13
+
+};  
+
+char keyMapRIGHTALT[numKeys] = {
+  KEY_F7,                KEY_F8,            KEY_F9,              KEY_F10,          KEY_F11,           KEY_F12,            '[',                    KEY_F13,
+  KEY_F13,          'y',            'u',              KEY_UP_ARROW,          'o',           'p',            ']',                    KEY_F13, 
+  KEY_BACKSPACE,     'h',            KEY_LEFT_ARROW,              KEY_DOWN_ARROW,          KEY_RIGHT_ARROW,           ';',             '\'',                    KEY_F13, 
+  KEY_DELETE,        'b',            'n',              'm',          ',',           '.',            '/',                    KEY_F13, 
+  KEY_RETURN,             ' ',          KEY_RIGHT_ALT,           KEY_F13,       KEY_F13,          KEY_F13,          '\\',                    KEY_F13
+};
+
+bool keyStateNP[numKeys] = {0};
+bool keyStateL[numKeys] = {0};
+bool keyStateR[numKeys] = {0};   
+
+bool isShiftPressed = false;
+bool isCapsLockOn = false;
+
 unsigned long lastPressTime;
-uint8_t lastKey;
-const int keyDelay = 500; 
+uint8_t lastKeyPressed = 255; 
+bool initialDelayPassed = false;
+
+uint8_t mouseSpeed =7; 
+int16_t x = 0;
+int16_t y = 0;
 //---------------------------------------------
 
 
-//-----------------PROTOTYPES------------------
-void pressKey(PCA9505_06 &expander, uint8_t pin, uint8_t key, uint8_t delay);
-//---------------------------------------------
-
-
-//------------------FUNCTIONS------------------
+//------------------FUNCTIONS------------------.................;;;;;....;;;;...........................
 /**
- * void setup(), init function
+ * @brief void setup(), init function
 */
 void setup() {
-  NUMPAD.begin(0x23);  //0100 011      
-  //LEFT.begin(0x22);  //0100 010
-  //RIGHT.begin(0x21); //0100 001
+  NUMPAD.begin(0x23);
+  LEFT.begin(0x22);
+  RIGHT.begin(0x21); 
+  trackball.begin(0x0A);
 
-  for(uint8_t i = 0; i<=NUM_KEYS_NUMPAD; i++){
+  for (uint8_t i = 0; i < numKeys; i++) {
     NUMPAD.pinMode(i, INPUT);
-  }
-  /*
-  for(uint8_t i = 0; i<=NUM_KEYS_LEFT; i++){
     LEFT.pinMode(i, INPUT);
-  }
-  for(uint8_t i = 0; i<=NUM_KEYS_RIGHT; i++){
     RIGHT.pinMode(i, INPUT);
   }
-  */
   
   Keyboard.begin();
-}
 
-/**
- * void loop(), infinite loop function
-*/
-void loop() {
-  for (uint8_t i = 0; i < NUM_KEYS_NUMPAD; i++) {
-    pressKey(NUMPADMAP[i].expander, NUMPADMAP[i].pin);
+  if(trackball.isConnected()) {
+    Mouse.begin();
   }
-  /*
-  for (uint8_t i = 0; i < NUM_KEYS_LEFT; i++) {
-    pressKey(LEFTMAP[i].expander, LEFTMAP[i].pin, LEFTMAP[i].key, keyRepeatDelay);
-  }
-  for (uint8_t i = 0; i < NUM_KEYS_RIGHT; i++) {
-    pressKey(RIGHTMAP[i].expander, RIGHTMAP[i].pin, RIGHTMAP[i].key, keyRepeatDelay);
-  }
-  */
-}
-
-/**
- * void presskey(), handler function to, well, duh, press keys.
- * @param expander the PCA9505 IO expander name used (NUMPAD, LEFT or RIGHT)
- * @param pin the pin number of the PCA (0 through 39)
-*/
-void pressKey(PCA9505_06* expander, uint8_t pin) {
-  uint8_t key = expander->digitalRead(pin) == LOW;
-  if (key != lastKey) {
-    lastPressTime = millis();
-    lastKey = key;
-    
-    if (key != 0) {
-      Keyboard.press(key);
-    } else {
-      Keyboard.releaseAll();
+  else {
+    while(true)
+    {
+      delay(1000);
     }
-  } else if (key != 0 && millis() - lastPressTime > keyDelay) {
-    Keyboard.write(key);
-    lastPressTime = millis();
+  }
+}
+
+
+/**
+ * @brief void loop(), infinite loop function
+*/
+void loop() { 
+  bool funcKeyPressed = ((LEFT.digitalRead(35)==0) || (RIGHT.digitalRead(8))==0)  ;
+  checkKeys(NUMPAD, keyMapNUMPAD, keyMapNUMPADALT, keyStateNP, funcKeyPressed);
+  checkKeys(LEFT, keyMapLEFT, keyMapLEFTALT, keyStateL, funcKeyPressed);
+  checkKeys(RIGHT, keyMapRIGHT, keyMapRIGHTALT, keyStateR, funcKeyPressed);
+
+  if(trackball.changed()){
+    y = (trackball.right() - trackball.left()) * mouseSpeed;
+    x = (trackball.down() - trackball.up())*(-1)* mouseSpeed;
+  if(x != 0 || y != 0){
+      Mouse.move(x, y, 0);
+  }
+  if(trackball.click()){
+      Mouse.press(MOUSE_LEFT);
+    }
+    else if(trackball.release()){
+      if(Mouse.isPressed(MOUSE_LEFT)){
+        Mouse.release(MOUSE_LEFT);
+      }
+    }
+  }
+}
+
+
+/**
+ * @brief Checks the state of each key connected to a specific PCA9505 chip, triggers key press/release actions, and handles mouse clicks.
+ *
+ * This function iterates over each key connected to the PCA9505 chip, represented by a PCA9505_06 object, and checks whether the state of the key has changed
+ * (i.e., it has been pressed or released). It triggers the appropriate key press/release actions based on the current state of the keys and supports handling
+ * mouse clicks. If a "function" key is pressed, the function switches to an alternative keymap layer defined by the function keymap.
+ *
+ * @param expander Reference to the PCA9505_06 object representing the PCA9505 chip to be checked.
+ * @param keyMap Pointer to the array containing the primary keymap for the PCA9505 chip.
+ * @param keyMapFunction Pointer to the array containing the function keymap for the PCA9505 chip.
+ * @param keyState Pointer to the array containing the current state of each key connected to the PCA9505 chip.
+ *
+ * @note The function keymap is activated when the "function" key, represented by FUNCTION_KEY, is pressed.
+ * @note MOUSE_LEFT_CLICK and MOUSE_RIGHT_CLICK are special constants used to represent mouse click actions.
+ */
+void checkKeys(PCA9505_06& expander, char* keyMap, char* funcKeyMap, bool* keyState, bool funcKeyPressed) {
+  for (uint8_t i = 0; i < numKeys; i++) {
+    bool isPressed = (expander.digitalRead(i) == 0);
+    char key = funcKeyPressed ? funcKeyMap[i] : keyMap[i];
+    if (isPressed != keyState[i]) {
+      keyState[i] = isPressed;
+      switch (key) {
+        case KEY_SCREENSHOT:
+          if (isPressed) {
+            Keyboard.press(KEY_LEFT_GUI);
+            Keyboard.press(KEY_LEFT_SHIFT);
+            Keyboard.press('s');
+            delay(50); 
+            Keyboard.releaseAll();
+            lastPressTime = millis();
+            lastKeyPressed = i;
+            initialDelayPassed = false;
+          }
+          break;
+
+        case KEY_CHEVRON_L:
+          if (isPressed) {
+            if (isCapsLockOn || isShiftPressed) {
+              Keyboard.press(KEY_LEFT_SHIFT); 
+              Keyboard.press(KEY_CHEVRON_L); 
+              Keyboard.releaseAll(); 
+            } else {
+              Keyboard.press(KEY_CHEVRON_R); 
+              Keyboard.release(KEY_CHEVRON_R); 
+            }
+            lastPressTime = millis();
+            lastKeyPressed = i;
+            initialDelayPassed = false;
+          } 
+          break;
+        case MOUSE_LEFT_CLICK:
+          isPressed ? Mouse.press(MOUSE_LEFT) : Mouse.release(MOUSE_LEFT);
+          break;
+        case MOUSE_RIGHT_CLICK:
+          isPressed ? Mouse.press(MOUSE_RIGHT) : Mouse.release(MOUSE_RIGHT);
+          break;
+        case MOUSE_SCROLL_UP: 
+          if (isPressed) Mouse.move(0, 0, 1); 
+          break;
+        case MOUSE_SCROLL_DOWN:
+          if (isPressed) Mouse.move(0, 0, -1); 
+          break;
+        default:
+          if (isPressed) {
+            Keyboard.press(key);
+            lastPressTime = millis();
+            lastKeyPressed = i;
+            initialDelayPassed = false;
+          } else {
+            Keyboard.release(key);
+            if (i == lastKeyPressed) lastKeyPressed = 255;
+          }
+          break;
+      }
+    }
+  }
+
+  if (lastKeyPressed != 255 && keyState[lastKeyPressed] &&
+      ((millis() - lastPressTime > INITIAL_DELAY && !initialDelayPassed) ||
+       (initialDelayPassed && millis() - lastPressTime > REPEAT_DELAY))) {
+    char key = funcKeyPressed ? funcKeyMap[lastKeyPressed] : keyMap[lastKeyPressed];
+    if (key != MOUSE_LEFT_CLICK && key != MOUSE_RIGHT_CLICK && key != MOUSE_SCROLL_UP && key != MOUSE_SCROLL_DOWN) {
+      Keyboard.release(key);
+      Keyboard.press(key);
+      lastPressTime = millis();
+      initialDelayPassed = true;
+    }
   }
 }
 //---------------------------------------------
+
+
+//END
